89aa8ee8086f4bea7ce1a8cf779af50fb0421b72
ryim-lv <ryim@learnvest.com>
2017-05-27 21:05:20 -0400

    add last question

--- question4/script.js
+++ question4/script.js
@@ -1,7 +1,18 @@
-function validateForm() {
-    var x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-        alert("Name must be filled out");
-        return false;
+var button = document.querySelector('.button')
+var headline = document.querySelector('.header')
+
+button.onclick = function validateForm() {
+    var passwordInput = document.form.password.value;
+    var usernameInput = document.form.username.value;
+    var integerValidation = new RegExp('([0-9])')
+
+    if(!integerValidation.test(usernameInput)) {
+      alert('incorrect')
+      return false;
     }
+    if(passwordInput !== '12345678') {
+      alert('incorrect');
+      return false;
+    }
+    headline.innerText = 'The information in the form is correct';
 }
